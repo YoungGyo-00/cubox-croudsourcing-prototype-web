c6d0e48427dab278124770dc9c9fb69f444c4814
Younggyo <lee4519698@hanmail.net>
2022-02-23 17:11:25 +0900

    [Server] Router 주석 생성

--- routers/auth.js
+++ routers/auth.js
@@ -11,8 +11,8 @@ router.use((req, res, next) => {
 
 router.post('/signup', isNotLoggedIn, Controller.signup); // 회원가입
 router.post('/certifications', Controller.certifications); // 휴대폰 인증 API endpoint
-router.post('/login', isNotLoggedIn, Controller.login);
-router.get('/logout', isLoggedIn, Controller.logout);
-router.get('/me', Controller.me); // 로그인 중 확인
+router.post('/login', isNotLoggedIn, Controller.login); // 로그인
+router.get('/logout', isLoggedIn, Controller.logout); // 로그아웃
+router.get('/me', Controller.me); // 로그인 상태 확인
 
 module.exports = router; 
--- routers/user.js
+++ routers/user.js
@@ -4,8 +4,8 @@ const { checkPermission } = require('../middlewares/user');
 const Controller = require('../controllers/user');
 const router = express.Router();
 
-router.get('/:nickName', Controller.GetUserInfo);
+router.get('/:nickName', Controller.GetUserInfo); // 유저 정보 조회
 /* router.patch('/:nickName', Controller.PatchUserInfo); */
-router.delete('/:nickName', checkPermission, Controller.DeleteUser);
+router.delete('/:nickName', checkPermission, Controller.DeleteUser); // 유저 삭제
 
 module.exports = router; 
